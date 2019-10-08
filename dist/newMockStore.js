73fc716f70b14852be78e48960142426b3175a49
Noa Rave <noa.rave@wikimedia.de>
2019-10-08 10:04:01 +0200

    newMockStore: add
    Copy of [0] and test
    
    [0] https://gerrit.wikimedia.org/r/plugins/gitiles/mediawiki/extensions/Wikibase/+/refs/heads/master/client/data-bridge/tests/unit/store/newMockStore.ts

---
+++ dist/newMockStore.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function newMockStore({ commit, dispatch, state, getters, rootState, rootGetters }) {
+    return {
+        commit: commit || jest.fn(),
+        dispatch: dispatch || jest.fn(() => Promise.resolve()),
+        state: state || {},
+        getters: getters || {},
+        rootState: rootState || {},
+        rootGetters: rootGetters || {},
+    };
+}
+exports.default = newMockStore;
