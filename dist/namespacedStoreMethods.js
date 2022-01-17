e5442896453cb280a37e70b4ce1ac8f19e5980b2
Michael Große <michael.grosse@wikimedia.de>
2022-01-17 17:37:11 +0100

    Update dependencies

--- dist/namespacedStoreMethods.js
+++ dist/namespacedStoreMethods.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.getter = exports.mutation = exports.action = void 0;
 /**
  * @param namespacesAndName namespace1, namespace2, ..., mutationOrActionOrGetterName
  */
